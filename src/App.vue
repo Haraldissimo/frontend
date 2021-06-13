48c434e206aca7c8097e87e7f186d8862feccc6f
Harald Weing <hw-201370@rwu.de>
2021-06-13 13:53:25 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <v-app id="app">
+    <h1>Professor Rating App</h1>
+    <AddEntry id="addEntry" @entryAdded="addEntry"></AddEntry>
+    <ListEntries
+      id="listEntry"
+      v-for="(singleEntry, index) of listOfEntries"
+      :key="index"
+      :entry="singleEntry"
+      :index="index"
+      @entryRemoved="removeEntry"
+      @entryEdited="editEntry"
+    ></ListEntries>
+    <button v-on:click="removeAll()">Remove all</button>
+  </v-app>
+</template>
+<script>
+import AddEntry from "./components/AddEntry.vue";
+import ListEntries from "./components/ListEntries.vue";
+import axios from "axios";
+export default {
+  name: "App",
+  components: {
+    AddEntry,
+    ListEntries
+  },
+  data: function() {
+    return {
+      listOfEntries: []
+    };
+  },
+  methods: {
+    addEntry: function(e) {
+      axios
+        .post("http://localhost:8080/profs/", {
+          name: e.name,
+          rating: e.rating
+        })
+        .then(response => {
+          this.listOfEntries = response.data;
+        });
+    },
+    editEntry: function(e) {
+      axios
+        .put("http://localhost:8080/profs/" + e.index, {
+          name: e.name,
+          rating: e.rating
+        })
+        .then(response => {
+          this.listOfEntries = response.data; //TODO: change this, do not return full list
+        });
+    },
+    removeEntry: function(e) {
+      axios.delete("http://localhost:8080/profs/" + e.index).then(response => {
+        this.listOfEntries = response.data;
+      });
+    },
+    removeAll: function() {
+      axios.delete("http://localhost:8080/profs/").then(response => {
+        this.listOfEntries =response.data;
+        this.listOfEntries =[];
+      });
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:8080/profs/").then(response => {
+      this.listOfEntries = response.data;
+    });
+  }
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 60px;
+  width: 700px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: lightblue;
+}
+#addEntry,
+h1 {
+  margin-bottom: 40px;
+}
+</style>
