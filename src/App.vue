54370bd12f7d8ae2e9c646fcf430a0261bd582d0
Haraldissimo <77877393+Haraldissimo@users.noreply.github.com>
2021-06-13 14:46:03 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -42,7 +42,7 @@ export default {
     },
     editEntry: function(e) {
       axios
-        .put("webprog2021.herokuapp.com/profs/" + e.index, {
+        .put("https://webprog2021.herokuapp.com/profs/" + e.index, {
           name: e.name,
           rating: e.rating
         })
@@ -51,19 +51,19 @@ export default {
         });
     },
     removeEntry: function(e) {
-      axios.delete("webprog2021.herokuapp.com/profs/" + e.index).then(response => {
+      axios.delete("https://webprog2021.herokuapp.com/profs/" + e.index).then(response => {
         this.listOfEntries = response.data;
       });
     },
     removeAll: function() {
-      axios.delete("webprog2021.herokuapp.com/profs/").then(response => {
+      axios.delete("https://webprog2021.herokuapp.com/profs/").then(response => {
         this.listOfEntries =response.data;
         this.listOfEntries =[];
       });
     }
   },
   mounted() {
-    axios.get("webprog2021.herokuapp.com/profs/").then(response => {
+    axios.get("https://webprog2021.herokuapp.com/profs/").then(response => {
       this.listOfEntries = response.data;
     });
   }
